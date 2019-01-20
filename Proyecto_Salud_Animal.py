9620ece53e499fb495f7d31a0073bcb863469f01
Dorival96 <46574838+dorival96@users.noreply.github.com>
2019-01-20 08:24:59 -0500

    Cargar imagen con Pil

--- Proyecto_Salud_Animal.py
+++ Proyecto_Salud_Animal.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from tkinter import ttk
-from reportlab.pdfgen import canvas
+from PIL import ImageTk,Image
+#from reportlab.pdfgen import canvas
 
 def operacion():
-
     r = Label(ventana, text="Ingrese el peso del animalito:").place(x=20, y=180)
     r1 = Entry(ventana, textvariable=peso).place(x=180, y=180)
     r2 = Label(ventana, text="Escoga la enfermedad: ").place(x=20, y=200)
@@ -27,24 +27,30 @@ def operacion():
         animal="Hamster"
         print(animal)
     r.config(state=DISABLED)
+
     combo.config(state=DISABLED)
     r1.config(state=DISABLED)
     r2.config(state=DISABLED)
 
 #pip install reportlab
-def generaPDF()
+def generaPDF():
     c = canvas.Canvas("hola-mundo.pdf")
     c.save()
     c.drawString(50, 50, "¡Hola, mundo!")
 
-
+#Aqui inicia la interfaz
 ventana = Tk()
 opcion=IntVar()
 num=IntVar()
 animal=StringVar()
 peso=StringVar()
 ventana.title("Salud Animal")
-ventana.geometry("350x550")
+ventana.geometry("500x500")
+
+#Código para cargar imagen utilizando Pil
+img = ImageTk.PhotoImage(Image.open("170511.jpg"))
+l=Label(image=img)
+l.pack()
 
 etiquetaTitulo = Label(ventana, text="Salud Animal").place(x=120, y=10)
 
