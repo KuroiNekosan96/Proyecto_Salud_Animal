53ad72dcc7c9459eee62bbe806f3fdbb61216fe2
DORIVAL <dpichamba@est.ups.edu.ec>
2019-02-03 18:29:26 -0500

    Funciones MenuBar y agregacion de campo Edad

--- Proyecto_Salud_Animal.py
+++ Proyecto_Salud_Animal.py
@@ -5,19 +5,32 @@ from tkinter import font
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4, letter
 
-
+def donothing():
+    print("0")
 #Metodo para limpiar variables
+
 def newFile():
     opcion.set("")
+    peso.set("")
+    edad.set("")
 
+def mostrarManual():
+    ventana2 = Tk()
+    ventana2.title("Salud Animal CRYP-Manual de Uso")
+    ventana2.geometry("370x370")
+    etiqueta2 = Label(ventana2, text="1.-Elegir una entre las cuatro opciones").place(x=80, y=80)
+    etiqueta3 = Label(ventana2, text="2.-Dar Clic en aceptar, se desplegará más opciones").place(x=80, y=120)
+    etiqueta4 = Label(ventana2, text="3.-Ingresar el peso de la mascota en gramos").place(x=80, y=160)
+    etiqueta5 = Label(ventana2, text="4.-Elegir una de las enfermedades especificadas").place(x=80, y=200)
+    etiqueta6 = Label(ventana2, text="5.-Ingresar la edad del perro").place(x=80, y=240)
+    etiqueta7 = Label(ventana2, text="6.-Dar clic en enviar resultados").place(x=80, y=280)
+    ventana2.mainloop()
 
-
-    # creacion del pdf
-    w, h = A4
-    c = canvas.Canvas("prueba.pdf", pagesize=A4)
-    c.drawString(50, h - 50, "¡Primer PDF!")
-    c.showPage()
-    #c.save()
+#Metodo para guardar variables falta la del combo
+def guardar():
+    animal=opcion.get();
+    peso=r.get();
+    edad=r2.get();
 
 
 def operacion():
@@ -31,13 +44,15 @@ def operacion():
     combo.place(x=230, y=300)
     combo['values'] = (
     'Seleccione', '1-Moquillo', '2-Rabia', '3-Gripe', '4-Desnutrucion','5-Infección por Campylobacter','6-La toxoplasmosis','7-La tiña')
-    combo.current(0)
-    boton = Button(ventana, text="Enviar Resultados", command=operacion, bg=colorFondo, font=Hefont).place(x=20, y=335)
+    etiqueta2 = Label(ventana, text="Ingresar Edad de la mascota:", bg=colorFondo, font=Helfont).place(x=20, y=340)
+    ed = Entry(ventana, textvariable=edad, font=Helvifont).place(x=230, y=340)
+    boton = Button(ventana, text="Enviar Resultados", command=operacion, bg=colorFondo, font=Hefont).place(x=20, y=370)
 
-    numero=num.get()
+
     if opcion.get()==1:
         animal="Gato"
         print("Ha elegido gato")
+
     elif opcion.get()==2:
         animal="Perro"
         print("Ha elegido perro")
@@ -52,6 +67,12 @@ def operacion():
 
 
 
+ # creacion del pdf
+w, h = A4
+c = canvas.Canvas("prueba.pdf", pagesize=A4)
+c.drawString(50, h - 50, "¡Primer PDF!")
+c.showPage()
+#c.save()
 
 
 
@@ -63,11 +84,18 @@ def operacion():
     c.drawString(50, 50, "¡Hola, mundo!")'''
 
 #Aqui inicia la interfaz
+#Variables Globales
 ventana = Tk()
+r=DoubleVar()
+peso=StringVar()
+enfermedad=StringVar()
+r2=IntVar()
 opcion=IntVar()
 num=IntVar()
-animal=StringVar()
-peso=StringVar()
+animal=IntVar()
+edad=IntVar()
+
+
 
 #Aqui inicia para la barra de herramientas
 
@@ -84,9 +112,13 @@ l.pack()
 
 menubar=Menu(ventana)
 filemenu=Menu(menubar,tearoff=0)
+filemenu1=Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Menú",men=filemenu)
 filemenu.add_command(label="New",command=newFile)
 filemenu.add_command(label="Exit",command=ventana.quit)
-menubar.add_cascade(label="File",men=filemenu)
+
+menubar.add_cascade(label="Informacion",men=filemenu1)
+filemenu1.add_command(label="Manual",command=mostrarManual)
 
 
 colorFondo="White"
@@ -100,8 +132,8 @@ Helfont=font.Font(family="Helvetica",size=10,weight="bold")
 etiqueta1=Label(ventana, text="Elige tu opción",bg=colorFondo,font=Helfont).place(x=20,y=80)
 xGato=Radiobutton(ventana, text="Gato", value=1, variable=opcion,bg=colorFondo,font=Helfont).place(x=20,y=120)
 xPerro=Radiobutton(ventana, text="Perro", value=2, variable=opcion,bg=colorFondo,font=Helfont).place(x=110,y=120)
-#xCanarios=Radiobutton(ventana, text="Canarios", value=3, variable=opcion,bg=colorFondo,font=Helfont).place(x=20,y=150)
-#xhamster=Radiobutton(ventana, text="Hamster", value=4, variable=opcion,bg=colorFondo,font=Helfont).place(x=110,y=150)
+xCanarios=Radiobutton(ventana, text="Canarios", value=3, variable=opcion,bg=colorFondo,font=Helfont).place(x=20,y=150)
+xhamster=Radiobutton(ventana, text="Hamster", value=4, variable=opcion,bg=colorFondo,font=Helfont).place(x=110,y=150)
 Hefont=font.Font(family="Helvetica",size=10,weight="bold")
 boton=Button(ventana,text="Aceptar",command=operacion,bg=colorFondo,font=Hefont).place(x=20,y=200)
 
